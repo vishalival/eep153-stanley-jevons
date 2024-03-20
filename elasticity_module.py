b3c8b28892c51845121a6ae608158f2706b72a63
Vishali Vallioor <vishalivallioor@wifi-10-43-149-191.wifi.berkeley.edu>
2024-03-19 18:39:40 -0700

    added elasticity info

---
+++ elasticity_module.py
@@ -0,0 +1,26 @@
+from cfe.estimation import drop_columns_wo_covariance
+import pandas as pd
+import numpy as np
+import cfe
+from cfe import Regression
+
+def output_as_pickle(expenditure_data, household_data, path):
+    x = pd.read_csv("data/ghana/Expenditures.csv")
+    d = pd.read_csv("data/ghana/Household_Characteristics.csv")
+    x.columns.name = 'j'
+    d.columns.name = 'k'
+    x = x.groupby('j',axis=1).sum()
+    x = x.replace(0,np.nan)
+    y = np.log(x.set_index(['i','t','m']))
+    d.set_index(['i','t','m'],inplace=True)
+    y = drop_columns_wo_covariance(y,min_obs=30)
+    use = y.index.intersection(d.index)
+    y = y.loc[use,:]
+    d = d.loc[use,:]
+    y = y.stack()
+    d = d.stack()
+    assert y.index.names == ['i','t','m','j']
+    assert d.index.names == ['i','t','m','k']
+    result = Regression(y=y,d=d)
+    result.predicted_expenditures()
+    result.to_pickle(path)
